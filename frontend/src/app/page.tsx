b9fca273dc1213d037a310782f8a5b39c8b318be
choi <minchoi9979@naver.com>
2025-03-19 16:32:46 +0900

    25-03-19 잘 되는지 확인

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,15 +3,14 @@ import Layout from "./layout"; // Layout 컴포넌트 가져오기
 
 import Header from "@/pages/header";
 import Footer from "@/pages/footer";
-import User from "@/pages/user";
 // import Intro from "@/pages/intro";
-// import Drips from "@/pages/drips";
+import Drips from "@/pages/drips";
 
 const Home: FC = () => {
   return (
     <Layout>
       <Header />
-      <User />
+      <Drips />
       <Footer />
     </Layout>
   );
